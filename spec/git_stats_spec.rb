e7a12b8faf9f3e974e4401bb8aa2b1dc2675de30
mishina <temma182008@gmail.com>
2021-01-27 23:06:39 +0900

    Make this spec a little better

--- spec/git_stats_spec.rb
+++ spec/git_stats_spec.rb
@@ -3,9 +3,7 @@ require 'spec_helper'
 describe GitStats do
   describe '.root' do
     it 'returns the project root path' do
-      # I don't know how to test this method correctly...
-      path = described_class.root
-      expect(path.join('spec/git_stats_spec.rb').to_s).to eq __FILE__
+      expect(described_class.root).to eq Pathname.new(File.expand_path('../', __dir__))
     end
   end
 end
