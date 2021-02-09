200c0f120ecb465ff44c7b24f6ef83a018836bf1
mishina <temma182008@gmail.com>
2021-02-10 07:34:09 +0900

    Add specs about gemspec
    Inspired by simplecov's spec: https://github.com/simplecov-ruby/simplecov/blob/main/spec/gemspec_spec.rb

--- spec/git_stats_spec.rb
+++ spec/git_stats_spec.rb
@@ -1,9 +1,30 @@
 require 'spec_helper'
+require 'open3'
 
 describe GitStats do
   describe '.root' do
     it 'returns the project root path' do
       expect(described_class.root).to eq Pathname.new(File.expand_path('../', __dir__))
+    end
+  end
+
+  describe 'gemspec sanity' do
+    after do
+      FileUtils.rm(Dir.glob('git_stats*.gem'))
+    end
+
+    let(:build) do
+      Bundler.with_original_env do
+        Open3.capture3('gem build git_stats.gemspec')
+      end
+    end
+
+    it 'has no warnings' do
+      expect(build[1]).not_to include('WARNING')
+    end
+
+    it 'succeeds' do
+      expect(build[2]).to be_success
     end
   end
 end
