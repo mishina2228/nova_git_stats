138900f78da511bcef0585ce87f78e8291a095ee
mishina <temma182008@gmail.com>
2021-10-27 18:52:46 +0900

    refactor: Update GitStats::Inspector
    * Introduce `ivars_to_be_filtered`
      * It can be used to indicate which variables should be filtered.
    * define `pretty_print` for `pp`
      * Since some Ruby version, irb uses `pp` by default.
    * Change `ivars_to_be_displayed` to private method.

--- lib/git_stats/inspector.rb
+++ lib/git_stats/inspector.rb
@@ -1,19 +1,52 @@
 module GitStats
   module Inspector
     def to_s
-      vars = ivars_to_be_displayed.map do |sym|
-        var = instance_variable_get(sym)
-        [sym, var.is_a?(String) ? %("#{var}") : var].join('=')
-      end.join(', ')
-      "#<#{self.class} #{vars}>"
+      vars = defined_ivars.map do |sym|
+        value = if ivars_to_be_filtered.include?(sym)
+                  '...'
+                else
+                  var = instance_variable_get(sym)
+                  var.is_a?(String) ? %("#{var}") : var.inspect
+                end
+        [sym, value].join('=')
+      end
+      vars = vars.join(', ').presence
+      "#<#{[self.class, vars].compact.join(' ')}>"
     end
+
+    def inspect
+      to_s
+    end
+
+    def pretty_print(pp)
+      pp.object_address_group(self) do
+        pp.seplist(defined_ivars, proc { pp.text(',') }) do |attr_name|
+          pp.breakable(' ')
+          pp.group(1) do
+            pp.text(attr_name.to_s)
+            pp.text('=')
+            if ivars_to_be_filtered.include?(attr_name)
+              pp.text('...')
+            else
+              pp.pp(instance_variable_get(attr_name))
+            end
+          end
+        end
+      end
+    end
+
+    private
 
     def ivars_to_be_displayed
       instance_variables
     end
 
-    def inspect
-      to_s
+    def ivars_to_be_filtered
+      []
+    end
+
+    def defined_ivars
+      (ivars_to_be_displayed | ivars_to_be_filtered).select { |sym| instance_variable_defined?(sym) }
     end
   end
 end
--- spec/git_stats/inspector_spec.rb
+++ spec/git_stats/inspector_spec.rb
@@ -3,11 +3,8 @@ require 'spec_helper'
 class Dummy
   include GitStats::Inspector
 
-  def initialize
-    super
-    @name = 'John Doe'
-    @count = 1
-    @created_at = Time.parse('2014-03-21 14:11:46 +0100')
+  def ivars_to_be_filtered
+    [:@password]
   end
 end
 
@@ -15,8 +12,61 @@ describe GitStats::Inspector do
   let(:dummy) { Dummy.new }
 
   describe '#to_s' do
-    it 'returns a class name and names and values of all instance variables' do
-      expect(dummy.to_s).to eq %(#<Dummy @name="John Doe", @count=1, @created_at=2014-03-21 14:11:46 +0100>)
+    context 'when the object has no instance variables' do
+      it 'returns only a class name' do
+        expect(dummy.to_s).to eq %(#<Dummy>)
+      end
+    end
+
+    context 'when the object has String instance variables' do
+      it 'returns a class name, names of instance variables, and their values enclosed in double quotes' do
+        dummy.instance_variable_set(:@name, 'John Doe')
+        expect(dummy.to_s).to eq %(#<Dummy @name="John Doe">)
+      end
+    end
+
+    context 'when the object has non-String instance variables' do
+      it 'returns a class name and names and values of instance variables' do
+        dummy.instance_variable_set(:@count, 1)
+        dummy.instance_variable_set(:@created_at, Time.parse('2014-03-21 14:11:46 +0100'))
+        expect(dummy.to_s).to eq %(#<Dummy @count=1, @created_at=2014-03-21 14:11:46 +0100>)
+      end
+    end
+
+    context 'when the object has nil instance variables' do
+      it 'returns a class name and names and values of instance variables' do
+        dummy.instance_variable_set(:@unused, nil)
+        expect(dummy.to_s).to eq %(#<Dummy @unused=nil>)
+      end
+    end
+
+    context 'when the object has false instance variables' do
+      it 'returns a class name and names and values of instance variables' do
+        dummy.instance_variable_set(:@flag, false)
+        expect(dummy.to_s).to eq %(#<Dummy @flag=false>)
+      end
+    end
+
+    context 'when the object has instance variables that are not defined in #ivars_to_be_displayed' do
+      it 'returns only a class name' do
+        allow(dummy).to receive(:ivars_to_be_displayed).and_return([])
+        dummy.instance_variable_set(:@age, 20)
+        expect(dummy.to_s).to eq %(#<Dummy>)
+      end
+    end
+
+    context 'when the object does not have instance variables defined in #ivars_to_be_displayed' do
+      it 'returns only a class name' do
+        allow(dummy).to receive(:ivars_to_be_displayed).and_return([:@name])
+        expect(dummy.to_s).to eq %(#<Dummy>)
+      end
+    end
+
+    context 'when the object has instance variables defined in #ivars_to_be_filtered' do
+      it 'returns a class name and names and filtered values of instance variables' do
+        dummy.instance_variable_set(:@password, 'password')
+        expect(dummy.to_s).to eq %(#<Dummy @password=...>)
+      end
     end
   end
 
@@ -28,7 +78,10 @@ describe GitStats::Inspector do
 
   describe '#ivars_to_be_displayed' do
     it 'returns all instance variable names' do
-      expect(dummy.ivars_to_be_displayed).to eq [:@name, :@count, :@created_at]
+      dummy.instance_variable_set(:@name, 'John Doe')
+      dummy.instance_variable_set(:@count, 1)
+      dummy.instance_variable_set(:@created_at, Time.parse('2014-03-21 14:11:46 +0100'))
+      expect(dummy.__send__(:ivars_to_be_displayed)).to match_array [:@name, :@count, :@created_at]
     end
   end
 end
